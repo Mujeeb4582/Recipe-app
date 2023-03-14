aee72c5e886d894ea606cf26dba5d8227589e6e0
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-14 23:35:55 +0500

    Revert "feat(users_helper.rb, create.html.erb, destroy.html.erb): add new files to the project"
    This reverts commit 4f30880f24a46896057ed316da5e84b57fe628dc.

--- db/migrate/20230313130319_create_foods.rb
+++ db/migrate/20230313130319_create_foods.rb
@@ -2,9 +2,9 @@ class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
       t.string :name
-      t.string :measurement_unit
+      t.decimal :measurement_unit, precision: 8, scale: 2
       t.decimal :price, precision: 8, scale: 2
-      t.decimal :quantity, precision: 8, scale: 2
+      t.integer :quantity
       t.references :user, null: false, foreign_key: { to_table: :users }
       t.timestamps
     end
