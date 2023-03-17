fa15511ce096b1dc2cc05d85985726047900970c
Hein Tay Zar <heintayzarhm@gmail.com>
2023-03-18 02:06:30 +0630

    Fix shopping list error

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -3,7 +3,7 @@ class ShoppingListController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_foods = RecipeFood.where(recipe_id: @recipe.id).includes(:food)
     @recipe_foods_with_names = @recipe_foods.map do |rf|
-      { id: rf.id, quantity: rf.quantity, recipe_id: rf.recipe_id, food_id: rf.food_id, name: rf.food.name }
+      { id: rf.id, quantity: rf.quantity, recipe_id: rf.recipe_id, food_id: rf.food_id, name: rf.food.name, price: rf.food.price }
     end
     @general_shopping_list, @total = current_user.general_shopping_list(@recipe_foods_with_names)
   end
