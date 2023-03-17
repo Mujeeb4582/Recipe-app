5c383aef967616ba80e30da3e082fbb3bd9ca2af
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-17 19:56:40 +0500

    feat(recipe_foods_controller_spec.rb): add tests for RecipeFoodsController actions (new, create, update, destroy)

---
+++ spec/requests/recipe_foods_controller_spec.rb
@@ -0,0 +1,104 @@
+require 'rails_helper'
+require_relative '../support/controller_auth'
+
+RSpec.describe RecipeFoodsController, type: :controller do
+  extend ControllerAuth
+  describe 'Testing routes' do
+    login_user
+    let(:user) { controller.current_user }
+    let(:recipe) { FactoryBot.create(:recipe, user: user) }
+    let(:food) { FactoryBot.create(:food) }
+    let(:recipe_food) { FactoryBot.create(:recipe_food, recipe: recipe, food: food) }
+
+    describe 'when user is logged in' do
+      it 'should return a 200 response' do
+        get :new, params: { user_id: user.id, recipe_id: recipe.id }
+        expect(response).to have_http_status(200)
+      end
+
+      it 'should have text: Add ingredient' do
+        get :new, params: { user_id: user.id, recipe_id: recipe.id }
+        expect(response.body).to include('Add ingredient')
+      end
+
+      describe 'POST #create' do
+        let(:user) { controller.current_user }
+        let(:valid_params) do
+          {
+            user_id: user.id,
+            recipe_id: recipe.id,
+            quantity: 2,
+            food_id: food.id
+          }
+        end
+        context 'when valid params are passed' do
+
+          it 'creates a new recipe food' do
+            expect {
+              post :create, params: valid_params
+            }.to change(RecipeFood, :count).by(1)
+          end
+        end
+
+        it 'redirects to the recipe page' do
+          post :create, params: valid_params
+
+          expect(response).to redirect_to(user_recipe_url(recipe.user, recipe))
+          expect(flash[:notice]).to eq('Ingredient has been added successfully!')
+        end
+    end
+      describe 'PUT #update' do
+      let(:user) { controller.current_user }
+      let(:id) { recipe_food.id}
+        let(:valid_params) do
+          {
+            user_id: user.id,
+            recipe_id: recipe.id,
+            quantity: 3,
+            food_id: food.id,
+            id: id
+          }
+        end
+        it 'updates the recipe food' do
+          put :update, params: valid_params
+
+          recipe_food.reload
+          expect(recipe_food.quantity).to eq(3)
+          expect(recipe_food.food).to eq(food)
+        end
+
+        it 'redirects to the recipe page' do
+          put :update, params: valid_params
+
+          expect(response).to redirect_to(user_recipe_url(recipe_food.recipe.user, recipe_food.recipe))
+          expect(flash[:notice]).to eq('Recipe food updated Successfully!')
+        end
+      end
+
+      describe 'DELETE #destroy' do
+      let(:valid_params) do
+        {
+          user_id: user.id,
+          recipe_id: recipe.id,
+          id: recipe_food.id
+        }
+      end
+        it 'deletes the recipe food' do
+          recipe_food
+
+          expect {
+            delete :destroy, params: valid_params
+          }.to change(RecipeFood, :count).by(-1)
+        end
+
+        it 'redirects to the recipe page' do
+          delete :destroy, params: valid_params
+
+          expect(response).to redirect_to(user_recipe_url(recipe.user, recipe))
+          expect(flash[:notice]).to eq('Ingredient deleted!')
+        end
+      end
+    end
+  end
+end
+
