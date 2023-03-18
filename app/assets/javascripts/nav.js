60913a07cac7c8d6e1e458c55d0ba1be6e148df6
Hein Tay Zar <heintayzarhm@gmail.com>
2023-03-18 22:50:57 +0630

    Remove console

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
         themeChanger.children[0].classList.toggle('hidden');
         themeChanger.children[1].classList.toggle('hidden');
         localStorage.setItem('darkMode', document.documentElement.classList.contains('dark'));
-        console.log(localStorage.getItem('darkMode'));
     }
 
     if (localStorage.getItem('darkMode') === 'true') {
