028802fb0da7d29d40694de4a8d53ba3a4def252
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-19 22:12:08 +0500

    correct the username

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :user_name =>     ENV['MAIL_USERNAME'],
-    :password =>    ENV['MAIL_PASSWORD'],
+    :user_name =>     "heintayzartech@gmail.com",
+    :password =>    "hevbblcnzmzzwgyq",
     :domain =>     "google.com",
     :address =>      'smtp.gmail.com',
     :port =>     587,
