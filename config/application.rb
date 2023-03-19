240244782855aeff1dbe414544aa051db51606d6
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-03-19 15:55:54 +0500

    feat(application.rb): add default_url_options for action_mailer with host https://recipe-app-jk5k.onrender.com

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module RecipeApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.action_mailer.default_url_options = { host: 'https://recipe-app-jk5k.onrender.com' }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
